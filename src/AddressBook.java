5e32721e45b5f2645f03db8f48fb37b50805ca77
adham <adham777ad@gmail.com>
2023-09-18 12:18:40 -0400

    i add functions call to test git commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,8 @@
 public class AddressBook {
     public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo();
+        AddressBook.add(buddy);
+        AddressBook.remove(buddy);
 
         System.out.println("Address Book");
     }
