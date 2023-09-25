067bb204d874677b9022a52cc697114eef5e9c4d
adham <adham777ad@gmail.com>
2023-09-25 11:04:51 -0400

    i add functions call to test git commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,9 +1,35 @@
+import java.util.ArrayList;
 public class AddressBook {
-    public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo();
-        AddressBook.add(buddy);
-        AddressBook.remove(buddy);
 
-        System.out.println("Address Book");
+    ArrayList<BuddyInfo> Buddy = new ArrayList<BuddyInfo>();
+    public AddressBook(){
+        Buddy =new ArrayList<>();
     }
+
+    private void addBuddy(BuddyInfo buddy) {
+        if(buddy !=null)
+            Buddy.add(buddy);
+    }
+
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < Buddy.size()) {
+            return Buddy.remove(index);
+        }
+        return null;
+
+
+    }
+
+
+
+
+    public static void main(String[] args){
+        BuddyInfo buddy = new BuddyInfo("tom", "carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
+    }
+
+
+
 }
